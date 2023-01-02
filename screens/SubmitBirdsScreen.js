1f9c52b9aad1ef3cadc4fb88d72ca62e42417ddd
Toby <tzitsman@gmail.com>
2023-01-01 20:23:46 -0500

    Update SubmitBirdsScreen.js

--- screens/SubmitBirdsScreen.js
+++ screens/SubmitBirdsScreen.js
@@ -1,30 +1,15 @@
 import { StyleSheet, Text, View, ScrollView  } from "react-native";
-import FormElement from "../components/FormElement";
-import ImageSelector from "../components/ImageSelector";
-import FindBirdsScreen from "./FindBirdsScreen";
+import Form from "../components/Form";
 
-const SubmitBirdsScreen = () => {
+const SubmitBirdsScreen = (props) => {
+    console.log(props)
     return(
         <ScrollView style={styles.scroll}>
             <View style={styles.container}>
                 <View style={styles.headerView}>
                     <Text style={styles.header}>Submit a Bird</Text>
                 </View>
-                <View style={styles.bodyView}>
-                    <View style={styles.formView}>
-                        <FormElement prompt='Species Name' />
-                    </View>
-                    <View style={styles.formView}>
-                        <FormElement prompt='Number Seen' />
-                    </View>
-                    <View style={styles.formView}>
-                        <FindBirdsScreen />
-                    </View>
-                    <View>
-                        <Text style={styles.header}>Upload a Picture?</Text>
-                        <ImageSelector />
-                    </View>
-                </View>
+                <Form />
             </View>
         </ScrollView>
     )
@@ -49,10 +34,6 @@ const styles = StyleSheet.create({
     headerView: {
         flex: 1
         
-    },
-    bodyView: {
-        flex: 6,
-        marginTop: 18,
     },
     scroll: {
         backgroundColor: 'brown'
