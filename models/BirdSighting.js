ddad832ba5b045608b01c1cf37898d3338a20612
Toby <tzitsman@gmail.com>
2023-01-06 10:27:50 -0500

    add ID field and separate lat and lng

--- models/BirdSighting.js
+++ models/BirdSighting.js
@@ -1,10 +1,12 @@
 export class BirdSighting {
-    constructor(species, count, location, time, date, imageUrl) {
+    constructor(species, count, location, time, date, imageUrl, id) {
         this.species = species,
         this.count = count,
         this.imageUrl = imageUrl,
-        this.location = location,
+        this.lat = location.lat,
+        this.lng = location.lng,
         this.time = time,
         this.date = date
+        this.id = id
     }
 }
