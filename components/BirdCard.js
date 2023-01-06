2dea4ce424d1f47988d7fc75effe70b448c81acc
Toby <tzitsman@gmail.com>
2023-01-05 19:42:27 -0500

    created files
    created outline of files for listing birds from database

---
+++ components/BirdCard.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View } from 'react-native'
+
+const BirdCard = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default BirdCard
+
+const styles = StyleSheet.create({
+    
+})
---
+++ components/SeenBirds.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View } from 'react-native'
+
+const SeenBirds = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default SeenBirds
+
+const styles = StyleSheet.create({
+    
+})
