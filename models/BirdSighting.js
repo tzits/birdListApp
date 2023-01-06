fcdba6f80ffa026d6f5b7ea4b5fca916f35fb8f6
Toby <tzitsman@gmail.com>
2023-01-05 19:45:04 -0500

    Model of Bird Sighting

---
+++ models/BirdSighting.js
@@ -0,0 +1,10 @@
+export class BirdSighting {
+    constructor(species, count, location, time, date, imageUrl) {
+        this.species = species,
+        this.count = count,
+        this.imageUrl = imageUrl,
+        this.location = location,
+        this.time = time,
+        this.date = date
+    }
+}
