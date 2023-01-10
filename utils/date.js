7631b9cda04d84d52352540cd0691dc364cdbf08
Toby <tzitsman@gmail.com>
2023-01-09 23:56:18 -0500

    date and time setting to utils

--- utils/date.js
+++ utils/date.js
@@ -6,3 +6,13 @@ export const setMinutes = (dateTime) => {
         return `${dateTime.getMinutes()}`
     }
 }
+
+export const setMyDate = (dateTime) => {
+    let date = `${dateTime.getDate()}/${dateTime.getMonth()+1}/${dateTime.getFullYear()}`
+    return date
+}
+
+export const setMyTime = (dateTime) => {
+    let time = `${dateTime.getHours()}:${setMinutes(dateTime)}`
+    return time
+}
