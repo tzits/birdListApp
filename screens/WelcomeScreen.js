4c8bac7f228b5f1ad827c15e6e7be42926310e16
Toby <tzitsman@gmail.com>
2023-01-14 21:19:25 -0500

    some more text to welcome

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -2,7 +2,12 @@ import { StyleSheet, Text } from "react-native"
 
 const WelcomeScreen = () => {
     return (
-        <Text>Welcome Screen</Text>
+        <>
+            <Text>Welcome To Your Personal BirdList</Text>
+            <Text>This app allows you to keep track on birds you see</Text>
+            <Text>It also looks for birds seen recently in your area</Text>
+            <Text>or on a place you select on a map</Text>
+        </>
     )
 }
 
