02ed21b63ad45f79535402f7120b1326756a05d8
Toby <tzitsman@gmail.com>
2023-01-19 10:34:25 -0500

    Error clean up

--- components/BirdList.js
+++ components/BirdList.js
@@ -26,7 +26,7 @@ const BirdList = ({data, onDeleteHandler}) => {
       <FlatList style={styles.container}
         data={data}
         renderItem={renderItem}
-        keyExtractor={(bird) => bird.id + bird.speciesCode}
+        keyExtractor={(bird) => bird.id + Math.random()}
         extraData={data}
       />
     )
